9df22688e3a914f6a952ecc131984b93024988b4
Youssef5107 <ybelalmarzouk@gmail.com>
2025-09-02 15:45:46 +0300

    style:add header content

---
+++ app/components/Header.tsx
@@ -0,0 +1,23 @@
+import styles from "../page.module.css"
+
+
+function Header() {
+    return (
+        <>
+            <header className={styles.headerArea}>
+                <div>
+                    <img src="assets/shared/logo.svg" alt="Page logo" />
+                </div>
+                <div className={styles.headerOptionBtnsArea}>
+                    <div className={styles.headerOptionBtn}><span className={styles.optionNumber}>00</span>HOME</div>
+                    <div className={styles.headerOptionBtn}><span className={styles.optionNumber}>01</span>DESTINATION</div>
+                    <div className={styles.headerOptionBtn}><span className={styles.optionNumber}>02</span>CREW</div>
+                    <div className={styles.headerOptionBtn}><span className={styles.optionNumber}>03</span>TECHNOLOGY</div>
+                </div>
+            </header >
+        </>
+    );
+
+}
+
+export default Header;
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Header from "./components/Header";
+import HomePage from "./Home/Home-page";  
 
 export default function Home() {
   return (
     <main>
-
+      <Header />
+      <HomePage />
     </main>
   );
 }
