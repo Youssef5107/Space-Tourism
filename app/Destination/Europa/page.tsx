6cdcd43bda94e86b65bb9b8f1539834932c1650c
Youssef5107 <ybelalmarzouk@gmail.com>
2025-09-06 16:06:24 +0300

    add destinatio page components

---
+++ app/Destination/Europa/page.tsx
@@ -0,0 +1,5 @@
+function Europa(){
+
+}
+
+export default Europa;
---
+++ app/Destination/Moon/page.tsx
@@ -0,0 +1,8 @@
+import styles from "../page.module.css"
+
+
+function Moon(){
+    <img src="assets/destination/image-moon.png" alt="" />
+}
+
+export default Moon;
