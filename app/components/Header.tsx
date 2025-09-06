1d741ead9ed18a1b5382eb356b1dc0987585db26
Youssef <ybelalmarzoukk@gmail.com>
2025-09-06 18:50:59 +0300

    adjust header links paths

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -10,8 +10,8 @@ function Header() {
                     <img src="/assets/shared/logo.svg" alt="Page logo" />
                 </div>
                 <div className={styles.headerOptionBtnsArea}>
-                    <Link href={"./"} className={styles.headerOptionBtn}><span className={styles.optionNumber}>00</span>HOME</Link>
-                    <Link href={"./Destination"} className={styles.headerOptionBtn}><span className={styles.optionNumber}>01</span>DESTINATION</Link>
+                    <Link href={"/"} className={styles.headerOptionBtn}><span className={styles.optionNumber}>00</span>HOME</Link>
+                    <Link href={"/Destination"} className={styles.headerOptionBtn}><span className={styles.optionNumber}>01</span>DESTINATION</Link>
                     <Link href={""} className={styles.headerOptionBtn}><span className={styles.optionNumber}>02</span>CREW</Link>
                     <Link href={""} className={styles.headerOptionBtn}><span className={styles.optionNumber}>03</span>TECHNOLOGY</Link>
                 </div>
