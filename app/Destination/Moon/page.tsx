b22fbf15c0713398e3ef0fe74ead57b3a5e29cf9
Youssef <ybelalmarzoukk@gmail.com>
2025-09-06 18:44:45 +0300

    adjust moon page

--- app/Destination/Moon/page.tsx
+++ app/Destination/Moon/page.tsx
@@ -6,22 +6,24 @@ function Moon(){
         <>
             <img src="/assets/destination/image-moon.png" alt="" />
             <div className={styles.pageMainContentArea}>
-                <div className={styles.mainContentTitle}>Moon</div>
-                <div className={styles.mainContent}>
-                    See our planet as you’ve never seen it before.
-                    A perfect relaxing trip away to help regain perspective and come back refreshed.
-                    While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.
-                </div>
-                <hr/>
-                <div className={styles.infoArea}>
-                    <div className={styles.distanceInfoArea}>
-                        <div className={styles.avgDistance}>AVG. DISTANCE</div>
-                        <div className={styles.distance}>384,400 KM</div>
+                <div>
+                    <div className={styles.mainContentTitle}>MOON</div>
+                    <div className={styles.mainContent}>
+                        See our planet as you’ve never seen it before.
+                        A perfect relaxing trip away to help regain perspective and come back refreshed.
+                        While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.
                     </div>
-                    <div className={styles.timeInfoArea}>
-                        <div>
-                            <div className={styles.estTime}>EST. TRAVEL TIME</div>
-                            <div className={styles.time}>3 DAYS</div>
+                    <hr/>
+                    <div className={styles.infoArea}>
+                        <div className={styles.distanceInfoArea}>
+                            <div className={styles.avgDistance}>AVG. DISTANCE</div>
+                            <div className={styles.distance}>384,400 KM</div>
+                        </div>
+                        <div className={styles.timeInfoArea}>
+                            <div>
+                                <div className={styles.estTime}>EST. TRAVEL TIME</div>
+                                <div className={styles.time}>3 DAYS</div>
+                            </div>
                         </div>
                     </div>
                 </div>
